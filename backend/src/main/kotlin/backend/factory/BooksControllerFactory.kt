bf0ef12728804f994d5435a024d8a4fc5077cb67
cdq73700 <cdq73700@gmail.com>
2023-03-26 11:38:46 +0900

    引数宣言修正

--- backend/src/main/kotlin/backend/factory/BooksControllerFactory.kt
+++ backend/src/main/kotlin/backend/factory/BooksControllerFactory.kt
@@ -3,15 +3,20 @@ package backend.factory
 import backend.BooksController
 import backend.manager.BooksStateManager
 import backend.service.BooksService
+import io.micronaut.context.annotation.Bean
+import io.micronaut.context.annotation.Factory
 
 /**
  * 書籍コントローラーファクトリー
  */
-public class BooksControllerFactory(service: BooksService, stateManager: BooksStateManager) {
-    private final val booksService: BooksService = service
-    private final val booksStateManager: BooksStateManager = stateManager
+@Factory
+public class BooksControllerFactory(
+    private final val service: BooksService,
+    private final val stateManager: BooksStateManager
+) {
 
+    @Bean
     public fun createBooksController(): BooksController {
-        return BooksController(booksService, booksStateManager)
+        return BooksController(service, stateManager)
     }
 }
