0a652b2d110a11ce01eea76cb95abbf420986f8c
cdq73700 <cdq73700@gmail.com>
2023-04-08 12:37:21 +0900

    .envを追加

--- backend/src/main/kotlin/backend/Application.kt
+++ backend/src/main/kotlin/backend/Application.kt
@@ -3,6 +3,21 @@ package backend
 import io.micronaut.runtime.Micronaut
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun readEnv() {
+    val rootPath = System.getProperty("user.dir")
+    val envFilePath = "$rootPath/.env"
+    if (Files.exists(Paths.get(envFilePath))) {
+        Files.readAllLines(Paths.get(envFilePath)).forEach { line ->
+            if (!line.startsWith("#") && line.contains("=")) {
+                val (key, value) = line.split("=", limit = 2)
+                System.setProperty(key, value)
+            }
+        }
+    }
+}
 
 @OpenAPIDefinition(
     info = Info(
@@ -12,8 +27,10 @@ import io.swagger.v3.oas.annotations.info.Info
     )
 )
 object Application {
+
     @JvmStatic
     fun main(args: Array<String>) {
+        readEnv()
         Micronaut.run(ApplicationFactory::class.java, *args)
     }
 }
